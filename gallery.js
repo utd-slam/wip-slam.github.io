e25d14ce9e5e47dd9a827d9f351ec66bfc602409
BrettLasater <133703919+BrettLasater@users.noreply.github.com>
2023-08-22 08:34:06 -0500

    Test
    Test for pulling website data stored in local CSV

---
+++ gallery.js
@@ -0,0 +1,37 @@
+const cellVariables = [];
+
+// URL of the published Google Sheet (replace with your URL)
+const sheetUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSiGx4ODFfVOOrQFe4xy-POn0ImXK5jpO1Dq9R1QW-QWEgB-ZPv_qj_d5NAQ31tf_ZmZ0yxinIxuXfX/pubhtml';
+
+// Fetch data from the published Google Sheet
+fetch(sheetUrl)
+.then(response => response.text())
+.then(data => {
+  const rows = data.split('\n');
+  
+  // Initialize an array to store variables for each cell
+  const cellVariables = [];
+  
+  // Iterate through rows
+  for (const row of rows) {
+    const columns = row.split(',');
+    
+    // Iterate through columns within the row
+    for (const columnData of columns) {
+      // Push each cell's data to the cellVariables array
+      cellVariables.push(columnData);
+    }
+  }
+  
+  // Now the cellVariables array contains data from each cell
+  console.log(cellVariables);
+
+  const outputElement = document.getElementById('output');
+  
+  // Update HTML elements using cellVariables
+  for (const cellData of cellVariables) {
+    const cellElement = document.createElement('p');
+    cellElement.textContent = cellData;
+    outputElement.appendChild(cellElement);
+  }
+});
